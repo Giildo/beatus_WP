f47821d89d89b51c4206b88012583d20e14de916
Giildo <giildo.jm@gmail.com>
2019-08-25 20:25:11 +0200

    Create design for page template.

--- page.php
+++ page.php
@@ -2,8 +2,29 @@
 
 get_header();
 $page = get_post();
+
+$content = preg_split('/\n/', preg_replace('/\r/', '', $page->post_content));
+
 ?>
+    <section id="page__section">
 
+        <nav>
+            <h2>Dernières actualités</h2>
+        </nav>
 
+        <section>
+            <h1><?= $page->post_title ?></h1>
+
+            <img src="<?= get_the_post_thumbnail_url($page->ID) ?>" alt="Photo de couverture de la page.">
+
+            <section>
+                <?php foreach ($content as $paragraph) :
+                    if ($paragraph !== ''):?>
+                        <p><?= $paragraph ?></p>
+                    <?php endif;
+                endforeach; ?>
+            </section>
+        </section>
+    </section>
 
 <?php get_footer();
