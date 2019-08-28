dbbdae6e01c76274ef4a6466408e75c31ceb7ee1
Giildo <giildo.jm@gmail.com>
2019-08-28 22:15:59 +0200

    Design page without post list

--- page.php
+++ page.php
@@ -2,6 +2,7 @@
 
 $pageID = get_the_ID();
 $categoryName = get_post()->post_name;
+$sectionIDs = [2721, 44, 336, 368];
 
 function tnCustomExcerptLength($length)
 {
@@ -22,56 +23,70 @@ remove_filter('the_excerpt', 'wpautop');
 ?>
     <section id="page__section">
 
-        <nav>
-            <h2>Dernières actualités</h2>
+        <?php if (in_array($pageID, $sectionIDs)): ?>
+            <nav>
+                <h2>Dernières actualités</h2>
 
-            <aside id="posts__cards">
+                <aside id="posts__cards">
+                    <?php
+                    $recentPosts = new WP_Query(
+                        [
+                            'category_name'  => "actualites-{$categoryName}",
+                            'posts_per_page' => 4
+                        ]
+                    );
+
+                    if ($recentPosts->post_count !== 0) :
+                        while ($recentPosts->have_posts()) : $recentPosts->the_post(); ?>
+                            <a href="<?php the_permalink(); ?>">
+                                <div class="post__card">
+                                    <header style="
+                                            background-image: url(<?= get_the_post_thumbnail_url(); ?>);
+                                            background-repeat: no-repeat;
+                                            background-size: cover;
+                                            background-position: center;
+                                            ">
+                                        <h5><?php the_title(); ?></h5>
+                                    </header>
+                                    <section>
+                                        <p><?php the_excerpt(); ?></p>
+                                    </section>
+                                </div>
+                            </a>
+                        <?php endwhile;
+                    else:?>
+                        <h3>Aucun article dans cette catégorie.</h3>
+                    <?php
+                    endif;
+                    wp_reset_query();
+                    ?>
+                </aside>
+            </nav>
+        <?php endif; ?>
+
+        <section <?php if (!in_array($pageID, $sectionIDs)) echo 'style="grid-column: 1/3;"'; ?>>
+            <?php
+            $recentPosts = new WP_Query(
+                [
+                    'page_id' => get_the_ID()
+                ]
+            );
+            if ($recentPosts->post_count !== 0) :
+                while ($recentPosts->have_posts()) : $recentPosts->the_post(); ?>
+                    <h1><?php the_title() ?></h1>
+
+                    <?php if (get_the_post_thumbnail_url()) : ?>
+                        <img src="<?= get_the_post_thumbnail_url() ?>" alt="Photo de couverture de la page.">
+                    <?php endif; ?>
+
+                    <section>
+                        <?php the_content(); ?>
+                    </section>
                 <?php
-                $recentPosts = new WP_Query(
-                    [
-                        'category_name'  => "actualites-{$categoryName}",
-                        'posts_per_page' => 4
-                    ]
-                );
-
-                if ($recentPosts->post_count !== 0) :
-                    while ($recentPosts->have_posts()) : $recentPosts->the_post(); ?>
-                        <a href="<?php the_permalink(); ?>">
-                            <div class="post__card">
-                                <header style="
-                                        background-image: url(<?= get_the_post_thumbnail_url(); ?>);
-                                        background-repeat: no-repeat;
-                                        background-size: cover;
-                                        background-position: center;
-                                        ">
-                                    <h5><?php the_title(); ?></h5>
-                                </header>
-                                <section>
-                                    <p><?php the_excerpt(); ?></p>
-                                </section>
-                            </div>
-                        </a>
-                    <?php endwhile;
-                else:?>
-                    <h3>Aucun article dans cette catégorie.</h3>
-                <?php
-                endif;
-                wp_reset_query();
-                ?>
-            </aside>
-        </nav>
-
-        <section>
-            <?php $page = get_post($pageID); ?>
-            <h1><?= $page->post_title ?></h1>
-
-            <?php if (get_the_post_thumbnail_url($page->ID)) : ?>
-                <img src="<?= get_the_post_thumbnail_url($page->ID) ?>" alt="Photo de couverture de la page.">
-            <?php endif; ?>
-
-            <section>
-                <?= $page->post_content ?>
-            </section>
+                endwhile;
+            endif;
+            wp_reset_query();
+            ?>
         </section>
     </section>
 
